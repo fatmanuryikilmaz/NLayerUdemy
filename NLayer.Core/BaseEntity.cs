f10741321684820d8668ee2e03098c9964d3daa2
fatmanuryikilmaz <125798606+fatmanuryikilmaz@users.noreply.github.com>
2024-01-10 17:49:03 +0300

    first commit

---
+++ NLayer.Core/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public abstract class BaseEntity
+    {
+        //In abstract, you cannot create a new object with the 'new' keyword.You can use it for the common things
+        //Abstract classes provide a way to define a common interface for a group of related classes while allowing each derived class to provide its own specific implementation. They are a key concept in object-oriented programming for achieving abstraction and polymorphism.
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ NLayer.Core/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public class Product:BaseEntity
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        //every single product has one category
+        public Category Category { get; set; }
+        //you can show if can take a null with a question mark in the end "?"
+
+        //application can work with or without ? mark
+        //you should use nullable feature for referance types
+        //or you can close the null check from properties of the core layer
+        public ProductFeature ProductFeature { get; set; }
+
+    }
+}
