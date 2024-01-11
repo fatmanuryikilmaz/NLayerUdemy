2ac685eee81fb3a04527145de1676180642a9003
fatmanuryikilmaz <125798606+fatmanuryikilmaz@users.noreply.github.com>
2024-01-11 18:34:57 +0300

    repository layer added

--- NLayer.Core/BaseEntity.cs
+++ NLayer.Core/BaseEntity.cs
@@ -12,6 +12,6 @@ namespace NLayer.Core
         //Abstract classes provide a way to define a common interface for a group of related classes while allowing each derived class to provide its own specific implementation. They are a key concept in object-oriented programming for achieving abstraction and polymorphism.
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
     }
 }
